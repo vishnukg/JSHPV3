d3cb28c69d13ec2c9385aa544cea8ef08b8da142
Vishnu Ganesan <vishnukg@gmail.com>
2025-11-29 14:31:03 +1100

    Added test coverage

---
+++ src/onsubmit.js
@@ -0,0 +1,13 @@
+export const onSubmit = (donation) => {
+  if (donation === "") {
+    return "Want to donate?";
+  }
+  const amount = Number(donation);
+  if (isNaN(amount) || amount < 0) {
+    return "Invalid input";
+  } else if (amount === 0) {
+    return "No donation, no problem";
+  } else if (amount > 0) {
+    return "Thanks for donation";
+  }
+};
