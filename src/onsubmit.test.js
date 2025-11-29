a17fcba11776adfc925e76f70849130773059598
Vishnu Ganesan <vishnukg@gmail.com>
2025-11-29 14:34:31 +1100

    Updated tests

--- src/onsubmit.test.js
+++ src/onsubmit.test.js
@@ -8,18 +8,27 @@ describe(" Onsubmit function", () => {
     assert.strictEqual(val, "No donation, no problem");
   });
 
-  it("when you pass 0 as a number", async () => {
+  it("when you pass 0 as a number", () => {
     const val = onSubmit(0);
     assert.strictEqual(val, "No donation, no problem");
   });
 
-  it("when you pass a number greater than 0", async () => {
+  it("when you pass a number greater than 0", () => {
     const val = onSubmit(10);
     assert.strictEqual(val, "Thanks for donation");
   });
 
-  it("when you pass an empty string for donation", async () => {
+  it("when you pass an empty string for donation", () => {
     const val = onSubmit("");
     assert.strictEqual(val, "Want to donate?");
+  });
+
+  it("when you pass an invalid number", () => {
+    const val = onSubmit("abc");
+    assert.strictEqual(val, "Invalid input");
+  });
+  it("when you pass a negative number", () => {
+    const val = onSubmit(-10);
+    assert.strictEqual(val, "Invalid input");
   });
 });
